44378d46a7456ff5ad985efa0959f5e88bb73317
gercon101 <gerryconway@iname.com>
2014-08-23 23:39:28 +0100

    Added print statements to R script

--- run_analysis.R
+++ run_analysis.R
@@ -10,6 +10,7 @@ if (!require("reshape2")) {
 
 require("reshape2")
 
+print("Loading feature and activity metadata...")
 
 # read and store feature labels and activity labels
 features = read.table('./features.txt',header=FALSE); 
@@ -18,11 +19,13 @@ activity_labels = read.table('./activity_labels.txt',header=FALSE);
 # assign meaningful column names to feature and activity_label data.frames
 names(features)<-c("Feature ID", "Feature")
 names(activity_labels)<-c("ActivityID", "Activity")
+print("Done")
 
 # assess the content of features and activity label data frames
 # str(features)
 # str(activity_labels)
 
+print("Loading test data...")
 # read and store test data (x_test), test subjects (subject_test) and test activity type data (y_test)
 x_test = read.table('./test/x_test.txt')
 subject_test = read.table('./test/subject_test.txt')
@@ -36,7 +39,12 @@ names(subject_test)<-c("SubjectID")
 names(y_test)<-c("Activity")
 # Replace the Activity IDs with the descriptive Activity Labels
 y_test[,1] = activity_labels[y_test[,1], 2]
+# Retain only the mean and standard deviation feature columns
+x_test<-x_test[,grep('mean|std',names(x_test))]
 
+print("Done")
+
+print("Loading training data...")
 # read and store training data (x_train), training subjects (subject_train) and training activity type data (y_train)
 x_train = read.table('./train/x_train.txt')
 subject_train = read.table('./train/subject_train.txt')
@@ -52,8 +60,9 @@ names(y_train)<-c("Activity")
 y_train[,1] = activity_labels[y_train[,1], 2]
 
 # Retain only the mean and standard deviation feature columns
-x_test<-x_test[,grep('mean|std',names(x_test))]
 x_train<-x_train[,grep('mean|std',names(x_train))]
+
+print("Done")
 
 # Remove brackets and dashes from column names in training and test data
 names(x_test) <- gsub("\\(|\\)", "", names(x_test))
@@ -61,6 +70,7 @@ names(x_test) <- gsub("-", " ", names(x_test))
 names(x_train) <- gsub("\\(|\\)", "", names(x_train))
 names(x_train) <- gsub("-", " ", names(x_train))
 
+print("Merge training and test data")
 # merge the feature, subject and activity columns into single 
 # data.frames for training and test
 clean_test<-cbind(subject_test, y_test,x_test)
@@ -69,12 +79,21 @@ clean_train<-cbind(subject_train, y_train,x_train)
 # Combine the training and test datasets
 merged_clean_data<-rbind(clean_test,clean_train)
 
+print("Done")
+
+print("Write merged data file")
 # Write the clean and merged data to file
 write.table(merged_clean_data, "./merged_clean_data.csv", sep=",", row.names=FALSE)
+print("Done")
 
+print("Create Summary Data Set")
 # Create a second summary dataset  with the average of each feature for each activity and subject
 merged_clean_data_melt <- melt(merged_clean_data,id=c("Activity", "SubjectID"),measure.vars=names(merged_clean_data)[c(-1,-2)])
 mean_features_by_subject_activity <- dcast(merged_clean_data_melt,  Activity +SubjectID ~ variable,mean)
+print("Done")
 
+print("Write the summary data file")
 # The summary dataset is written to file
 write.table(mean_features_by_subject_activity, "./mean_features_by_subject_activity.csv", sep=",", row.names=FALSE)
+print("Done")
+print("Script Complete")
